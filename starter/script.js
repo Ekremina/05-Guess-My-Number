9caae82bcacf23f18761a72cf1c87a78772811c1
Ekremina Arik <ekreminaarik@gmail.com>
2021-05-16 17:46:00 +0100

    setting up highscore

--- starter/script.js
+++ starter/script.js
@@ -13,6 +13,7 @@ console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +29,13 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    //setting up the highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
     //when guess too high
   } else if (guess > secretNumber) {
     if (score > 1) {
